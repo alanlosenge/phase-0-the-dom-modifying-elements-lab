642b9d0b32a3e6171675bb7587603f2db0a1d63c
Allan Losenge <temlosenge@gmail.com>
2023-03-19 14:43:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+
+const yourName = 'Allan Losenge'; 
+newHeader.textContent = `${yourName} is the champion`;
+ 
+
+const body = document.querySelector('body');
+body.appendChild(newHeader);
